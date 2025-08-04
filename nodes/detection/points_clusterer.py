dd9639ccbc95a33647a7fb5614429138d86ceeb3
jim <294862048a@gmail.com>
2025-08-04 14:04:19 +0300

    practice 5-2, 5-3

--- nodes/detection/points_clusterer.py
+++ nodes/detection/points_clusterer.py
@@ -19,7 +19,7 @@ class PointsClusterer:
         self.clusterer = DBSCAN(eps=self.cluster_epsilon, min_samples=self.cluster_min_size)
 
         # Publishers
-
+        self.points_pub = rospy.Publisher('points_clustered', PointCloud2, queue_size=1, tcp_nodelay=True)
 
         # Subscribers
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2 ** 24,
@@ -30,10 +30,27 @@ class PointsClusterer:
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
         labels = self.clusterer.fit_predict(points)
 
-        print('points shape:', points.shape)
-        print('labels shape:', labels.shape)
+        # print('points shape:', points.shape)
+        # print('labels shape:', labels.shape)
 
         assert points.shape[0] == labels.shape[0], 'points and labels not same count'
+
+        points_labeled = np.hstack((points, labels.reshape(-1, 1)))
+        mask = labels != -1
+        points_labeled = points_labeled[mask]
+
+        # convert labelled points to PointCloud2 format
+        data = unstructured_to_structured(points_labeled, dtype=np.dtype([
+            ('x', np.float32),
+            ('y', np.float32),
+            ('z', np.float32),
+            ('label', np.int32)
+        ]))
+
+        # publish clustered points message
+        cluster_msg = msgify(PointCloud2, data)
+        cluster_msg.header = msg.header
+        # self.points_pub.publish(cluster_msg)
 
     def run(self):
         rospy.spin()
